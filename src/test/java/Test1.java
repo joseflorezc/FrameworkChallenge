ba982d523dea1d14e37bf3648c810b94007f4d92
jose manuel florez <jm.florezc@uniandes.edu.co>
2021-08-27 13:56:13 -0500

    aniadiendo el proyecto y primer avance

---
+++ src/test/java/Test1.java
@@ -0,0 +1,27 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.get("https://api.themoviedb.org/3/movie/550?api_key=2e172e18c00b19621224226c2fff60f9");
+
+        System.out.println(response.getBody().asString());
+
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+
+    }
+
+    @Test
+    public void test2(){
+        given().get("https://api.themoviedb.org/3/movie/550?api_key=2e172e18c00b19621224226c2fff60f9").then().statusCode(200);
+    }
+}
